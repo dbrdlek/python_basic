57a9e4c0b739a7b10dfc0928d49b3f898a9ffec8
Minwoo <o_o8583@nate.com>
2020-01-19 17:46:58 +0900

    DateType Test

---
+++ 02_python_dateType/05_dateType_Test_Q.py
@@ -0,0 +1,101 @@
+# Print 함수로 결과 출력
+# 파이썬 데이터 타입(자료형)
+# 딕셔너리, 집합 자료형
+# 데이터 타입 관련 퀴즈
+
+# 1. 아래 문자열의 길이를 구해보세요.
+q1 = "dk2jd923i1jdk2jd93jfd92jd918943jfd8923"
+
+# print('q1의 길이는 : %s ' % len(q1))
+print('1. ', len(q1))
+
+# 2. print 함수를 사용해서 아래와 같이 출력해보세요.
+#    apple;orange;banana;lemon
+print("2. " , """apple;orange;banana;lemon""")
+
+
+# 3. 화면에 * 기호 100개를 표시하세요.
+print('3. ' ,'*' * 100)
+
+
+# 4. 문자열 "30" 을 각각 정수형, 실수형, 복소수형, 문자형으로 변환해보세요.
+str_1 = '30'
+print('4. ', int(str_1))
+print('4. ', float(str_1))
+print('4. ', complex(str_1))
+print('4. ', str(30))
+
+# 5. 다음 문자열 "Niceman" 에서 "man" 문자열만 추출해보세요.
+str_1 = 'Niceman'
+# man으로 시작하는 인덱스를 찾는다
+str_1_idx = str_1.index("man")
+print('5. ', str_1[4:])
+print('5. ',str_1[str_1_idx:str_1_idx+3])
+
+# 6. 다음 문자열을 거꾸로 출력해보세요. : "Strawberry"
+str_1 = 'Strawberry'
+print('6. ', list(reversed(str_1)))
+print('6. ', str_1[::-1])
+
+# 7. 다음 문자열에서 '-'를 제거 후 출력하세요. : "010-7777-9999"
+str_1 = "010-7777-9999"
+print('7. ', str_1[0:3] + str_1[4:8] + str_1[9:13])
+print('7. ', str_1.replace('-', ''))
+# 정규표현식
+import re
+print("7. ", re.sub('[^0-9]','',str_1))
+
+# 8. 다음 문자열(URL)에서 "http://" 부분을 제거 후 출력하세요. : "http://daum.net"
+str_1 = "http://daum.net"
+print('8. ', str_1[7:])
+print('8. ', str_1.replace("http://", ''))
+
+# 9. 다음 문자열을 모두 대문자, 소문자로 각각 출력해보세요. : "NiceMan"
+str_1 = "NiceMan"
+print('9. ', str_1.upper())
+print('9. ', str_1.lower())
+
+# 10. 다음 문자열을 슬라이싱을 이용해서 "cde"만 출력하세요. : "abcdefghijklmn"
+str_1 = 'abcdefghijklmn'
+print('10. ', str_1[2:5])
+
+# 11. 다음 리스트에서 "Apple" 항목만 삭제하세요. : ["Banana", "Apple", "Orange"]
+l = ["Banana", "Apple", "Orange"]
+# l.remove("Apple")
+del l[1]
+print('11. ', l)
+
+# 12. 다음 튜플을 리스트로 변환하세요. : (1,2,3,4)
+t = (1,2,3,4)
+# l = list(t)
+print('12. ', list(t))
+# print('12. ', l)
+
+# 13. 다음 항목을 딕셔너리(dict)으로 선언해보세요. : <성인 - 100000 , 청소년 - 70000 , 아동 - 30000>
+dic = {
+	"성인": 100000,
+	"청소년": 70000,
+	"아동": 30000
+}
+dic_1 = {}
+dic_1['성인'] = 100000
+dic_1['청소년'] = 70000
+dic_1['아동'] = 30000
+
+print('13. ', dic)
+print('13. ', dic_1)
+
+# 14. 13번 에서 선언한 dict 항목에 <소아 - 0> 항목을 추가해보세요.
+dic["소아"] = 0
+print('14. ', dic)
+
+# 15. 13번에서 선언한 딕셔너리(dict)에서 Key 항목만 출력해보세요.
+print('15. ', dic.keys())
+print('15. ', list(dic.keys()))
+
+# 16. 13번에서 선언한 딕셔너리(dict)에서 value 항목만 출력해보세요.
+print('16. ', dic.values())
+print('16. ', list(dic.values()))
+print('16. ', tuple(dic.values()))
+
+# *** 결과 값만 정확하게 출력되면 됩니다. ^^* 고생하셨습니다. ***
