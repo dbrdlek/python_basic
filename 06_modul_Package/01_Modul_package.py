3c5311900c639c9d7c9adb23d49b7115f7ab3870
Minwoo <o_o8583@nate.com>
2020-01-22 23:21:09 +0900

    prints 파일 안에 단위테스트 추가

--- 06_modul_Package/01_Modul_package.py
+++ 06_modul_Package/01_Modul_package.py
@@ -4,3 +4,23 @@
 # 상대 경로
 # .. : 부모 디렉토리
 # .  : 현재 디렉토리
+
+# 폴더를 다르게 구분해놔서 한번에 import가 안돼 
+# os, sys에 path를 추가함
+# 아니면 이 폴더 안에 pkg폴더가 있으면 사용 가능함
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+# import sys
+# print(sys.path)
+
+# 사용1(클래스)
+from pkg.fibonacci import Fibonacci
+# from pkg.calculations import calculations
+
+Fibonacci.fib(300)
+
+
+
+
+
